29002a0c83301d26694e0fe692ee385c55479e3c
Evgeniya Zhukova <evgeniya.zhukova@georgebrown.ca>
2021-04-09 18:04:06 -0400

    Use @Input

--- missionlist/src/app/missiondetails/missiondetails.component.ts
+++ missionlist/src/app/missiondetails/missiondetails.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Mission } from "../models/mission";
 
 @Component({
   selector: 'app-missiondetails',
   templateUrl: './missiondetails.component.html',
   styleUrls: ['./missiondetails.component.css']
 })
+
 export class MissiondetailsComponent implements OnInit {
+  @Input() mission?: Mission;
 
   constructor() { }
 
